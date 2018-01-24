b15de12b8553ddc8d1086c4969145b0f05f8c8ae
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2018-01-24 16:14:10 -0600

    Create spinalCase.js

---
+++ spinalCase.js
@@ -0,0 +1,17 @@
+//Another rather messy experiment with RegEx. The goal is to take a string representing a sentence or phrase using
+//various kinds of word breaks, uncapitalize all words, and replace those breaks with "-". A bit too ad-hoc.
+
+function spinalCase(str) {
+  //If the break is '-', a capital letter, or a space, add another break.
+  var newstr = str.replace(/(-|[A-Z]|\s)/g, " $&");
+  //If the break is an underscore, get rid of it.
+  newstr = newstr.replace(/_/g,"");
+  //make everything lower-case.
+  newstr = newstr.toLowerCase();
+  //Get rid of the first space.
+  newstr = newstr.replace(/^\s*/,'');
+  //Using the spaces we added, split the string into words.
+  var arr = newstr.split(/\s+/);
+  //Get rid of any extra dashes (for dashes used as word-breaks.)
+  return arr.join("-").replace(/-+/g,'-');
+}
