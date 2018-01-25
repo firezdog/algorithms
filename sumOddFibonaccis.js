38382e0cebf74f65d2c0d25c381447b04d75390a
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2018-01-24 21:32:18 -0600

    Create sumOddFibonaccis.js

---
+++ sumOddFibonaccis.js
@@ -0,0 +1,18 @@
+//Function to return the sum of all odd Fibonacci numbers less than a given number.  The function
+//is supposed, first, to create an array of all Fibonacci numbers up to the given number
+//and, second, to loop through all elements of the array less than the given number, adding together
+//those that happen to be odd.
+
+function sumFibs(num) {
+    var fibs = [1,1];
+    for (var i = 1; fibs[i] <num; i++){
+        fibs.push(fibs[i-1]+fibs[i]);    
+    }
+    var sum = 0;
+    for (i = 0; fibs[i] <= num; i++) {
+      if (fibs[i] %2 == 1) {
+        sum += fibs[i];
+    }
+  }
+  return sum;
+}
