e89c6e5ab602121d24c22668d72415edb40f9c86
Alex Leibowitz <firezdog@gmail.com>
2021-05-15 21:17:25 -0700

    add notes for heap?

--- triple_sums/triple_sums.py
+++ triple_sums/triple_sums.py
@@ -8,13 +8,13 @@ def find_max_product(arr):
         # not sure if this is the cleanest implementation -- might be a recursive implementation that generalizes
         if item < largest[0]:
             if item <= largest[1]:
-                largest[0] = largest[1]
-                largest[1] = item
+                largest[0] = largest[1] # this would be swapping the parent and child
+                largest[1] = item # this would be replacing the child with the item
         if item > largest[0]:
             if item >= largest[2]:
-                largest[0] = largest[1]
-                largest[1] = largest[2]
-                largest[2] = item
+                largest[0] = largest[1] # this would be swapping the parent with the child
+                largest[1] = largest[2] # this would be swapping the child with the sibling
+                largest[2] = item # this would be swapping the child with the item
 
         if idx < 2:
             output.append(-1)
