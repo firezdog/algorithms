e3b549bf846dfb6e3f50316d01d28238c1b8f701
Alex Leibowitz <firezdog@gmail.com>
2021-05-15 21:14:47 -0700

    sort of non-generalized solution seems to be working

--- triple_sums/triple_sums.py
+++ triple_sums/triple_sums.py
@@ -1,11 +1,26 @@
 from functools import reduce
 
 def find_max_product(arr):
-    largest = [-1, -1, -1]
+    largest = [1, 1, 1]
     output = []
 
-    for item in arr:
-        product = reduce(lambda acc, curr: acc * curr, largest)
-        print(product)
+    for idx, item in enumerate(arr):
+        # not sure if this is the cleanest implementation -- might be a recursive implementation that generalizes
+        if item < largest[0]:
+            if item <= largest[1]:
+                largest[0] = largest[1]
+                largest[1] = item
+        if item > largest[0]:
+            if item >= largest[2]:
+                largest[0] = largest[1]
+                largest[1] = largest[2]
+                largest[2] = item
 
-find_max_product([1,2,1,2])
+        if idx < 2:
+            output.append(-1)
+        else:
+            output.append(reduce(lambda acc, x: acc * x, largest))
+
+    return output
+
+find_max_product([1,2,3,4,5,6])
