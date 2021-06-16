7dfd350cdbc4436ed522096ae22a0fc6b30cf746
Alex Leibowitz <alex.leibowitz@zoro.com>
2021-06-16 11:06:38 -0700

    setting up for maxpop algo

--- leetcode/max_pop_year/Solution.py
+++ leetcode/max_pop_year/Solution.py
@@ -2,5 +2,22 @@ from typing import List
 
 
 class Solution:
+    def __init__(self, logs):
+        self.solution = self.maximumPopulations(logs)
+
     def maximumPopulations(self, logs: List[List[int]]) -> int:
+        if len(logs) < 0: return 0
+        chronolog = sorted(logs, key=lambda log: log[0])
+        
+        deaths = [chronolog[0][1]] # idea -- pop deaths off as they become relevant, otherwise keep a queue
+        current_pop = 0
+        max_pop = 0
+        
+        for log in chronolog:
+            pass
+
         return 0
+
+data = [[1950, 1961], [1960, 1971], [1970, 1981]]
+test = Solution(logs=data)
+print(test.solution)
