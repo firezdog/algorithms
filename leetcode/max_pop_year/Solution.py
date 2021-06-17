6b00c254932eaddabef27d0007771543698788e5
Alex Leibowitz <alex.leibowitz@zoro.com>
2021-06-17 09:36:25 -0700

    solution does not currently return earliest year

--- leetcode/max_pop_year/Solution.py
+++ leetcode/max_pop_year/Solution.py
@@ -2,31 +2,37 @@ from typing import List
 
 
 class Solution:
-    def __init__(self, logs):
-        self.solution = self.maximumPopulation(logs)
-
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         if len(logs) < 0: return 0
-        chronolog = sorted(logs, key=lambda log: log[0])
+
+        logs_by_birth = sorted(logs, key=lambda log: log[0])
+        logs_by_death = sorted(logs, key=lambda log: log[1])
         
-        deaths = [] # idea -- pop deaths off as they become relevant, otherwise keep a queue
-        next_death = chronolog[0][1]
+        next_death = logs_by_death[0][1]
+        next_death_ptr = 1
+
         current_pop = 0
         max_pop = 0
+        max_pop_year = 0
         
-        for log in chronolog:
+        for log in logs_by_birth:
             current_pop += 1
-            deaths.append(log[1])
 
             now = log[0]
-            if next_death <= now: 
+            while next_death <= now: 
                 current_pop -= 1
-                next_death = deaths.pop(0) # this won't be correct, b/c there is no order among the deaths and pop will be out of whack
+                next_death_ptr += 1
+                try:
+                    next_death = logs_by_death[next_death_ptr][1]
+                except:
+                    break
 
-            if max_pop < current_pop: max_pop = current_pop
+            if max_pop < current_pop:
+                max_pop = current_pop
+                max_pop_year = now
 
-        return max_pop  # TODO: needs to return the year
+        return max_pop, max_pop_year
 
-data = [[1950, 1961], [1961, 1971]]
-test = Solution(logs=data)
-print(test.solution)
+data = [[1987,2047],[1952,2006],[2021,2042],[2047,2049],[2036,2040],[1994,2009]]
+test = Solution()
+print(test.maximumPopulation(logs=data)) # not sure if this is absolutely wrong, but in terms of constraints it is wrong b/c it does not return the earliest year
